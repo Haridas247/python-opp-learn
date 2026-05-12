b70cdda7e0efa0092ef906aa211dbb53a1bda1f7
hari <haarid3837@gmail.com>
2026-05-12 10:50:34 +0530

    add file and demo code

---
+++ constructor.py
@@ -0,0 +1,10 @@
+class user:
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
+
+        user1 = user ("Hari", "harid@gmail.com")
+        user2 = user ("Das","har@gmail.com")
+
+        print(user1.email)
+        print(user2.name)
