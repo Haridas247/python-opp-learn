8006d0c3b7f0ade33713f25631693c43d7355eb8
hari <haarid3837@gmail.com>
2026-05-12 10:30:55 +0530

    add file

---
+++ object.py
@@ -0,0 +1,14 @@
+class user:
+    pass
+user1 = user()
+user2 = user()
+user3 = user()
+
+print (user1)
+print (user2)
+print (user3)   
+
+# what i learned : 1.object is created by calling classname()
+#2.each object is seperate - they don't mix
+#3.one class can make many object
+#4. each object gets it own memory space
